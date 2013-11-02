07860a5c139de9d17bb48bf6ecaf0bea5e79dd57
Andrew Carpenter <andrew@audioinhd.com>
2013-11-02 00:30:19 -0400

    positioned header better, added some text

--- javascripts/ilikeux.js
+++ javascripts/ilikeux.js
@@ -1,15 +1,42 @@
 (function ($, window, document) {
 
+    function boxesText() {
+        $(".box").each(function (i) {
+            if (i === 0) {
+                this.innerHTML = "this";
+            } else if (i === 11) {
+                this.innerHTML = "is";
+            } else if (i === 22) {
+                this.innerHTML = "just";
+            } else if (i === 33) {
+                this.innerHTML = "an";
+            } else if (i === 44) {
+                this.innerHTML = "excuse";
+            } else if (i === 55) {
+                this.innerHTML = "to";
+            } else if (i === 66) {
+                this.innerHTML = "play";
+            } else if (i === 77) {
+                this.innerHTML = "around";
+            } else if (i === 88) {
+                this.innerHTML = "with";
+            } else if (i === 99) {
+                this.innerHTML = "js";
+            }
+        });
+    }
+
     function boxBuilder() {
-        var i, boxes_wrapper, box_width, box_height, colors, border;
+        var i, boxes_wrapper, wrapper_height, box_width, box_height, colors, border;
         i = 0;
         boxes_wrapper = $("#boxes");
-        box_width = boxes_wrapper.outerWidth() / 12 - 10;
-        box_height = boxes_wrapper.outerHeight() / 10 - 10;
+        wrapper_height = boxes_wrapper.outerHeight() - ($("header").outerHeight() + 4);
+        box_width = boxes_wrapper.outerWidth() / 10 - 10;
+        box_height = wrapper_height / 10 - 10;
         boxes_wrapper.empty();
         colors = ["havelock", "malibu", "nevada", "edward", "pomegranate"];
 
-        while (i < 120) {
+        while (i < 100) {
             border = colors[Math.floor(Math.random() * colors.length)];
             boxes_wrapper.append('<div class="box box-' + border + '"></div>');
             boxes_wrapper.find('.box').css({
@@ -18,6 +45,11 @@
             });
             i = i + 1;
         }
+        boxesText();
+    }
+
+    function boxesStart() {
+        $("#boxes").css("top", ($("header").outerHeight() + 8) + "px");
     }
 
     function fontScaling(font_factor) {
@@ -33,16 +65,16 @@
         } else {
             fontScaling(125);
         }
+        boxesStart();
+        boxBuilder();
     }
 
     $(window).on("resize", function () {
         sizing();
-        boxBuilder();
     });
 
     $(document).ready(function () {
         sizing();
-        boxBuilder();
     });
 
 })(jQuery, window, document);
